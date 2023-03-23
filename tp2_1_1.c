5870883bd47320f34082c59e500fc62aa090820e
facundo <salcedocristianit@gmail.com>
2023-03-23 18:35:27 -0300

    agregar .gitignore

--- tp2_1_1.c
+++ tp2_1_1.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 // codigo a completar
 #define N 20
@@ -8,10 +10,13 @@ int main(){
     int i;
     double vt[N];
 
-    for(i = 0;i<N; i++)
-    {
+    srand(time(NULL));
+
+    for(i = 0;i<N; i++){
+
     vt[i]=1+rand()%100;
     printf("%f", vt[i]);
+
     }
 
     return 0;
